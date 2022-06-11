04d92aede24934f4bbe782454986d5f5303d2b98
Dustin Rouillard <code@dstn.to>
2022-06-11 00:48:39 -0600

    Adding server branch

--- .profile
+++
@@ -1,5 +0,0 @@
-export GOPATH=$HOME/.gopath
-export PATH=$PATH:$GOPATH/bin
-
-export EDITOR=/usr/bin/vim
-export TERM=xterm-256color
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update apt and install zsh, git, curl
+apt update -y
+apt install zsh git curl -y
+
+# FzF fuzzy reverse search
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Copy over our theme, hushlogin and our .zsh files
+cp .zsh_theme ~/.oh-my-zsh/themes/dstn.zsh-theme
+cp .zshrc .zsh_funcs .zsh_kube .hushlogin ~
