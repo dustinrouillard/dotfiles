9985807ebca38ba65fb3bedb5654217dabe7aac5
Dustin Rouillard <code@dstn.to>
2024-03-05 15:52:17 -0700

    Add puff to mozilla domainsuffixwhitelist

--- .mozilla/firefox/dstn.default/user.js
+++ .mozilla/firefox/dstn.default/user.js
@@ -1,4 +1,5 @@
 //
 user_pref("browser.fixup.domainsuffixwhitelist.hop", true);
+user_pref("browser.fixup.domainsuffixwhitelist.puff", true);
 user_pref("browser.fixup.domainsuffixwhitelist.kush", true);
 user_pref("browser.fixup.domainsuffixwhitelist.dstn", true);
--- .zshrc.d/0-paths.zsh
+++ .zshrc.d/0-paths.zsh
@@ -1,6 +1,7 @@
 
 # Path configuration
 
+export PATH=$PATH:${KREW_ROOT:-$HOME/.krew}/bin
 export PATH=$PATH:$HOME/.bin
 export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:/usr/local/bin
