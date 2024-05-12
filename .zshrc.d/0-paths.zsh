71f008281a28e6d5a1d9c70b65682ef61a3d6acf
Dustin Rouillard <code@dstn.to>
2024-05-12 02:54:29 -0600

    Adding java 21 to path

--- .zshrc.d/0-paths.zsh
+++ .zshrc.d/0-paths.zsh
@@ -5,3 +5,4 @@ export PATH=$PATH:${KREW_ROOT:-$HOME/.krew}/bin
 export PATH=$PATH:$HOME/.bin
 export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:/usr/local/bin
+export PATH=/usr/lib/jvm/zulu-21/bin:$PATH
